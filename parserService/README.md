d37620ca2294a3b770c0834d11193ce8e53a90a1
Dante Mateo Bado Sucamele <dantebado@gmail.com>
2019-11-14 17:30:11 -0300

    keywords

--- parserService/README.md
+++ parserService/README.md
@@ -3,3 +3,6 @@ conda activate apiparser
 pip install flask flask-jsonpify flask-sqlalchemy flask-restful
 pip install requests
 pip install tika==1.19
+pip install numpy
+pip install spacy
+python -m spacy download es
--- parserService/parser.py
+++ parserService/parser.py
@@ -3,6 +3,12 @@ import urllib.request
 import requests
 from tika import parser
 from flask import Flask, make_response
+import os
+from collections import OrderedDict
+import numpy as np
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+import array
 
 apiURL = "http://localhost"
 apiPort = "6570"
@@ -35,12 +41,121 @@ def getFileContent(documentId):
 	parsedPDF = parser.from_file("documents/" + documentName)
 	content= parsedPDF["content"]
 	
+	os.remove("documents/" + documentName)
+	
 	return content
 
+def getKeyWords(documentId):
+	
+	nlp = spacy.load('es_core_news_sm')
+	
+	class TextRank4Keyword():		
+		def __init__(self):
+			self.d = 0.85 # damping coefficient, usually is .85
+			self.min_diff = 1e-5 # convergence threshold
+			self.steps = 10 # iteration steps
+			self.node_weight = None # save keywords and its weight		
+		def set_stopwords(self, stopwords):  
+			for word in STOP_WORDS.union(set(stopwords)):
+				lexeme = nlp.vocab[word]
+				lexeme.is_stop = True		
+		def sentence_segment(self, doc, candidate_pos, lower):
+			sentences = []
+			for sent in doc.sents:
+				selected_words = []
+				for token in sent:
+					# Store words only with cadidate POS tag
+					if token.pos_ in candidate_pos and token.is_stop is False:
+						if lower is True:
+							selected_words.append(token.text.lower())
+						else:
+							selected_words.append(token.text)
+				sentences.append(selected_words)
+			return sentences			
+		def get_vocab(self, sentences):
+			vocab = OrderedDict()
+			i = 0
+			for sentence in sentences:
+				for word in sentence:
+					if word not in vocab:
+						vocab[word] = i
+						i += 1
+			return vocab		
+		def get_token_pairs(self, window_size, sentences):
+			token_pairs = list()
+			for sentence in sentences:
+				for i, word in enumerate(sentence):
+					for j in range(i+1, i+window_size):
+						if j >= len(sentence):
+							break
+						pair = (word, sentence[j])
+						if pair not in token_pairs:
+							token_pairs.append(pair)
+			return token_pairs			
+		def symmetrize(self, a):
+			return a + a.T - np.diag(a.diagonal())		
+		def get_matrix(self, vocab, token_pairs):
+			vocab_size = len(vocab)
+			g = np.zeros((vocab_size, vocab_size), dtype='float')
+			for word1, word2 in token_pairs:
+				i, j = vocab[word1], vocab[word2]
+				g[i][j] = 1
+			g = self.symmetrize(g)
+			norm = np.sum(g, axis=0)
+			g_norm = np.divide(g, norm, where=norm!=0) # this is ignore the 0 element in norm			
+			return g_norm		
+		def get_keywords(self, number=10):
+			node_weight = OrderedDict(sorted(self.node_weight.items(), key=lambda t: t[1], reverse=True))
+			items = []
+			for i, (key, value) in enumerate(node_weight.items()):
+				data = {}
+				data["name"] = key
+				data["value"] = str(value)
+				items.append(data)
+				if i > number:
+					break
+			return json.dumps(items)
+		def analyze(self, text, 
+					candidate_pos=['NOUN', 'PROPN'], 
+					window_size=4, lower=False, stopwords=list()):
+			self.set_stopwords(stopwords)
+			
+			doc = nlp(text)
+			sentences = self.sentence_segment(doc, candidate_pos, lower)
+			vocab = self.get_vocab(sentences)
+			token_pairs = self.get_token_pairs(window_size, sentences)
+			g = self.get_matrix(vocab, token_pairs)
+			pr = np.array([1] * len(vocab))
+			previous_pr = 0
+			for epoch in range(self.steps):
+				pr = (1-self.d) + self.d * np.dot(g, pr)
+				if abs(previous_pr - sum(pr))  < self.min_diff:
+					break
+				else:
+					previous_pr = sum(pr)
+			node_weight = dict()
+			for word, index in vocab.items():
+				node_weight[word] = pr[index]			
+			self.node_weight = node_weight
+			
+	text = getFileContent(documentId)
+			
+	tr4w = TextRank4Keyword()
+	tr4w.analyze(text, candidate_pos = ['NOUN', 'PROPN'], window_size=4, lower=False)
+	return tr4w.get_keywords(10)
+
 @app.route('/documents/<documentId>/contents')
-def index(documentId):
+def route_contents(documentId):
 	response = make_response(getFileContent(documentId), 200)
-	response.mimetype = "text/plain"
+	response.mimetype = "application/json"
+	return response
+@app.route('/documents/<documentId>/keywords')
+def route_keywords(documentId):
+	keywords = getKeyWords(documentId)
+	print(type(keywords))
+	print(keywords)
+	response = make_response(keywords, 200)
+	response.mimetype = "application/json"
 	return response
 
 app.run(host='0.0.0.0', port=4242)
