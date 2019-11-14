3698b8bff2c08f53c3ed97223147cf0db6ad9e10
Dante Mateo Bado Sucamele <dantebado@gmail.com>
2019-11-14 16:45:31 -0300

    Added authorization

--- parserService/parser.py
+++ parserService/parser.py
@@ -9,12 +9,28 @@ apiPort = "6570"
 
 app = Flask(__name__)
 
-def getFileContent(documentId):	
-	response = requests.get(apiURL + ":" + apiPort + "/documents/" + documentId)	
+def getToken():
+	headers = {'Content-Type': 'application/json'}
+	data = {'credential': 'userAdmin', 'password':'utn.frba.nlp'}
+	r = requests.post(apiURL + ":" + apiPort + "/api/auth/signin", data=json.dumps(data), headers=headers)
+	
+	return json.loads(r.text)["access_token"]
+
+def getFileContent(documentId):
+	token = getToken();
+	
+	headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
+	print(headers)
+	response = requests.get(apiURL + ":" + apiPort + "/documents/" + documentId, headers=headers)
+	
 	y = json.loads(response.content)
 	documentURI = y["document_uri"]
-	documentName = y["document_name"]
+	documentName = y["document_name"]	
 	
+	opener = urllib.request.build_opener()
+	opener.addheaders = [('Authorization', 'Bearer ' + token)]
+	urllib.request.install_opener(opener)
+
 	urllib.request.urlretrieve(documentURI, "documents/" + documentName)
 	parsedPDF = parser.from_file("documents/" + documentName)
 	content= parsedPDF["content"]
@@ -26,5 +42,5 @@ def index(documentId):
 	response = make_response(getFileContent(documentId), 200)
 	response.mimetype = "text/plain"
 	return response
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=4242)
+
+app.run(host='0.0.0.0', port=4242)
